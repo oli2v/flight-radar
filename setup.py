981940ade07f718fc94cf373801ab9ed229936d4
Olivier Valenduc <olivier.valenduc@exalt-value.com>
2024-03-11 11:40:14 +0100

    find packages

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="flight-radar",
-    version="1.0.4",
+    version="1.0.5",
     description="This module implements an ELT Pipeline from FlightRadar API in a GCP environment.",
     author="Olivier Valenduc",
     author_email="o.valenduc@gmail.com",
-    packages=["flight_radar"],
+    packages=find_packages(),
     install_requires=[
         "FlightRadarAPI",
         "tqdm",
